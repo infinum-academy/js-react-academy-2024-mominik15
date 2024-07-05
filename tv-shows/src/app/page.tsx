311b09c799c034e06c427a90a7e69904fa231532
mominik15 <dominik.mikulcic2@gmail.com>
2024-07-05 23:16:30 +0200

    Project setup

---
+++ tv-shows/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "@chakra-ui/react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Heading>Neki tekst!</Heading>
+    </main>
+  );
+}
