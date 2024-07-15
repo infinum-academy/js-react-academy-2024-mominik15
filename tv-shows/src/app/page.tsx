4fe1e8617d8224e363d2850565d5340f42543e9f
mominik15 <dominik.mikulcic2@gmail.com>
2024-07-15 18:18:44 +0200

    Redirect from home to all shows

--- tv-shows/src/app/page.tsx
+++ tv-shows/src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import styles from "./page.module.css";
-import { ShowsList } from "@/components/shared/ShowsList/ShowsList";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-        <ShowsList topRated={false} />
-    </main>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/all-shows');
+  }, [router]); 
+
+  return null;
 }
